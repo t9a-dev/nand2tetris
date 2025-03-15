2e5f85a1b8b26f8cacdb1146a01de8fef45c56e0
t9a <t9a.dev@icloud.com>
2025-03-15 16:07:16 +0900

    Chapter4 乗算(Multi.asm)の実装

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,48 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+//sum=0
+@sum
+M=0
+
+//i=R1
+@R1
+D=M
+@i
+M=D
+
+//LOOP
+(LOOP)
+//if(i<=0) goto STOP
+@i
+D=M
+@STOP
+D;JLE
+
+//sum=sum+R0
+@R0
+D=M
+@sum
+M=M+D
+
+//i=i-1
+@i
+M=M-1
+
+// goto LOOP
+@LOOP
+0;JMP
+
+//STOP
+(STOP)
+
+//R2=sum
+@sum
+D=M
+@R2
+M=D
+
+//END
+(END)
+@END
+0;JMP
